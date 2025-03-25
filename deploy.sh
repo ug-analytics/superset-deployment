44691217402fbd8c43343c2c8bb183caf5171768
Türker Ziya Ercin <51151883+tzercin@users.noreply.github.com>
2025-03-25 11:44:25 +0300

    Init: Prepare for production (#1)
    * Fix: buildspec.yaml
    
    * Fix: Docker images, dependencies
    
    * Fix: values.yaml
    
    * Add: ingress yaml is applied
    
    * Add: node selectors

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 kubectl create namespace superset
-helm upgrade --install --values values.yaml superset superset/superset --namespace superset
+helm upgrade --install --values ./staging/values.yaml superset superset/superset --namespace superset
