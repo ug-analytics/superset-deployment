7b37c23e2b339f91e3eadb7270e1d09c80d4eed0
Turker Ziya Ercin <turker@userguiding.com>
2025-07-29 13:14:41 +0300

    Add: staging configurations completed

--- staging/deploy.sh
+++ staging/deploy.sh
@@ -1,2 +1,3 @@
 kubectl create namespace superset
+kubectl apply -f ./staging/secrets/secrets.yaml
 helm upgrade --install --values ./staging/values.yaml superset superset/superset --namespace superset
